3933efd37a89bec3e8dd528da1237d539f3aa3c9
ReneKriest <rkriest@gmail.com>
2014-08-04 08:09:19 +0200

    initial CLI git commit

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,3 +1,6 @@
-define(['type'], function (getString) {
-    alert (getString('rene'));
+define(['getString'], function (getString) {
+    var reneString = getString('rene');
+
+    document.querySelector('#display_name').innerText = eval(reneString);
+    document.querySelector('#display_code').innerText = reneString;
 });
